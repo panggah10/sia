bd7e0ff036ba6beddc3b2b8364c71c0f0e8335b6
unknown <panggah.widiandana10@gmail.com>
2022-11-26 11:30:26 +0700

    update 261122

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -9,7 +9,7 @@
                         <div class="d-flex justify-content-center py-4">
                             <a href="index.html" class="logo d-flex align-items-center w-auto">
                                 <img src="assets/img/logoistek.png" alt="">
-                                <span class="d-none d-lg-block">NiceAdmin</span>
+                                <span class="d-none d-lg-block">Welcome Muda Mulia</span>
                             </a>
                         </div><!-- End Logo -->
 
